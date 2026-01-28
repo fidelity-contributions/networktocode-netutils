7a89f183cf7b7be5105cb97e538fd2cecfbbba8a
Madhukumar Basavaraja <madhukumar.basavaraja@fmr.com>
2026-01-28 10:35:05 -0500

    Fixed aruba_os running configuration command from 'show running-configuration' to 'show running-config' in the RUNNING_CONFIG_MAPPER.
    Signed-off-by: Madhukumar Basavaraja <madhukumar.basavaraja@fmr.com>

--- netutils/lib_mapper.py
+++ netutils/lib_mapper.py
@@ -314,7 +314,7 @@ RUNNING_CONFIG_MAPPER: t.Dict[str, str] = {
     "adva_fsp150f2": "show running-config",
     "adva_fsp150f3": "show running-config delta",
     "arista_eos": "show running-config",
-    "aruba_os": "show running-configuration",
+    "aruba_os": "show running-config",
     "aruba_osswitch": "show running-config",
     "aruba_procurve": "show running-config",
     "checkpoint_gaia": 'clish -c "show configuration"',
